8f0a7d6e4b9f2aeafa8864e33063f7732d1ec4a1
SergiuSorocean10 <sorocean.370@gmail.com>
2024-03-26 00:59:17 +0200

    Revert "Reapply "Company getall and getByName added, Apply to job added""
    This reverts commit a811e1da832aad112ed51d7fc27584f0925b71cc.

--- JobPortalAPI/Services/Interaces/IApplicationService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace JobPortalAPI.Services.Interaces
-{
-    public interface IApplicationService
-    {
-        public Task<string> ApplyToJob(int jobID, string coverLetter, IFormFile cv);
-        public Task<string> CancelApplication();
-    }
-}
